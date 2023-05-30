1579f3838d5bf01068f173de2906e37b1f08f4b8
Keisuke Takagi <mit@takagi.app>
2023-05-31 00:52:17 +0900

    update: updated accepted props

--- apps/web/app/components/atoms/LinkButton/index.tsx
+++ apps/web/app/components/atoms/LinkButton/index.tsx
@@ -1,20 +1,38 @@
 import Link from "next/link";
 import { forwardRef } from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css"
 
 type Props = {
   disabled?: boolean;
+  variant?: "small" | "medium" | "large";
+  weight?: "normal" | "bold";
 } & React.ComponentPropsWithoutRef<typeof Link>;
 
 const LinkButton = forwardRef<HTMLAnchorElement, Props>(function LinkButtonBase({
   children,
   href,
   disabled,
+  variant = "medium",
+  weight = "bold",
   ...rest
 }, ref) {
   return (
     <Link href={href} {...rest} legacyBehavior>
-      <a ref={ref} aria-disabled={disabled ? "true" : "false"}>
-        {children}
+      <a
+        className={clsx(styles["module"])} 
+        ref={ref} 
+        aria-disabled={disabled ? "true" : "false"}
+        data-variant={variant}
+        data-weight={weight}
+      >
+        {
+          typeof children === "string" ? (
+            children.toUpperCase()
+          ) : (
+            children
+          )
+        }
       </a>
     </Link>
   )
